a8fec7493c843fbc885299ab4b5718e1429704ca
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-27 21:44:36 +0300

    add styles

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Task } from '../Task/Task';
 import './TaskList.scss';
 
 export const TaskList = () => {
   return (
-    <div className="hero">
-    </div>
+    <ul className="task-list">
+      <Task />
+      <button
+        type="button"
+        className="task-list__button"
+      >Clear All</button>
+    </ul>
   );
 }
