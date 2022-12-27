f2b6f194a62536981a77fab7e13a439e70b6d4c4
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-27 21:43:26 +0300

    add task, styles

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -3,7 +3,11 @@ import './Task.scss';
 
 export const Task = () => {
   return (
-    <div className="hero">
-    </div>
+    <li className="task">
+      <button className="task__button task__button--done">=</button>
+      <p className="task__text">Buy x-mas presents</p>
+      <button className="task__button task__button--edit">!</button>
+      <button className="task__button task__button--delete">-</button>
+    </li>
   );
 }
