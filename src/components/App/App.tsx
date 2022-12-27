036bf54317e0c82e8fea57764a4881e3b05775d2
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-27 20:35:10 +0300

    add app styles, basic styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { AddTask } from '../AddTask/AddTask';
+import { TaskList } from '../TaskList/TaskList';
 import './App.scss';
 
 const App = () => {
   return (
     <div className="hero">
-      <h1 className="hero__title">Do ToDo</h1>
+      <h1 className="headline">Do ToDo</h1>
+      <AddTask />
+      <TaskList />
     </div>
   );
 }
