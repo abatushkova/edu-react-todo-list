02df88f07d0d8afb0de7261689df66a53dbda27c
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-27 20:35:54 +0300

    add add-task structure, styles

--- src/components/AddTask/AddTask.tsx
+++ src/components/AddTask/AddTask.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddTask.scss';
 
 export const AddTask = () => {
+  const [text, setText] = useState('');
+
+  const handleAddClick = () => {
+    setText('');
+  }
+
   return (
-    <div className="hero">
+    <div className="field">
+      <input
+        type="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+        className="field__input"
+        placeholder="Add new to-do"
+      />
+      <button
+        type="button"
+        onClick={handleAddClick}
+        className="field__button"
+      >+</button>
     </div>
   );
 }
