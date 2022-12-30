14c69f45791b8bc9b1e7cda4b93bd072ca42cdc7
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-30 22:41:06 +0300

    add task provider

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { AddTask } from '../AddTask/AddTask';
 import { TaskList } from '../TaskList/TaskList';
+import { TaskProvider } from '../../context/TaskProvider';
 import './App.scss';
 
 const App = () => {
   return (
     <div className="hero">
       <h1 className="headline">Do ToDo</h1>
-      <AddTask />
-      <TaskList />
+      <TaskProvider>
+        <AddTask />
+        <TaskList />
+      </TaskProvider>
     </div>
   );
 }
