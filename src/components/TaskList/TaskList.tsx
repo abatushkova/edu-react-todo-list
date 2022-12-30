c28dc91222b7baea695755ece4d88119030d8147
Alena Batushkova <alena.batushkova@gmail.com>
2022-12-30 22:45:11 +0300

    add styles, add context hook, dispatch clear action

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { Task } from '../Task/Task';
+import { useTasks, useTasksDispatch } from '../../context/TaskProvider';
 import './TaskList.scss';
 
 export const TaskList = () => {
+  const tasks = useTasks();
+  const dispatch = useTasksDispatch();
+
+  const handleCLearAllClick = () => {
+    dispatch({
+      type: 'clear',
+      payload: [],
+    });
+  };
+
   return (
     <ul className="task-list">
-      <Task />
-      <button
-        type="button"
-        className="task-list__button"
-      >Clear All</button>
+      {tasks.map(task => (
+        <Task key={task.id} task={task} />
+      ))}
+      {tasks.length > 0 && (
+        <button
+          type="button"
+          className="task-list__button"
+          onClick={handleCLearAllClick}
+        >Clear All</button>
+      )}
     </ul>
   );
 }
